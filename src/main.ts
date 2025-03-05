5360967cbc1797d2a4b2e3d24ca44b6f64439c41
alegs01 <ale.gomez.saez@gmail.com>
2025-03-05 00:40:25 -0300

    Se añade archivo para manejar errorer globales que no sean manejadas por filtros especificos.
    La idea es añadir en un futuro más errores globales especificos,

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { setupSwagger } from './swagger/swagger.config';
+import { GlobalExceptionFilter } from './filters/global-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
-
+  app.useGlobalFilters(new GlobalExceptionFilter());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
