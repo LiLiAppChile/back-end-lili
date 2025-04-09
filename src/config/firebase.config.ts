0396a1d8c30ca3f42877e63c82a816d0cde2f62e
Diego Cabrera <162330383+diegocab27@users.noreply.github.com>
2025-04-09 17:33:36 -0400

    Revert "Feature/assign service"

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,24 +1,21 @@
 import * as admin from 'firebase-admin';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
+import * as serviceAccount from './serviceAccountKey.json';
 
 const firebaseParams = {
-  type: process.env.FIREBASE_TYPE,
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: process.env.FIREBASE_AUTH_URI,
-  token_uri: process.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+  type: serviceAccount.type,
+  projectId: serviceAccount.project_id,
+  privateKeyId: serviceAccount.private_key_id,
+  privateKey: serviceAccount.private_key,
+  clientEmail: serviceAccount.client_email,
+  clientId: serviceAccount.client_id,
+  authUri: serviceAccount.auth_uri,
+  tokenUri: serviceAccount.token_uri,
+  authProviderX509CertUrl: serviceAccount.auth_provider_x509_cert_url,
+  clientC509CertUrl: serviceAccount.client_x509_cert_url,
 };
 
 admin.initializeApp({
-  credential: admin.credential.cert(firebaseParams as admin.ServiceAccount),
+  credential: admin.credential.cert(firebaseParams),
 });
 
 const db = admin.firestore();
--- src/reviews/reviews.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ReviewsService } from './services/reviews.service';
-import { ReviewsController } from './reviews.controller';
-
-@Module({
-  providers: [ReviewsService],
-  controllers: [ReviewsController],
-})
-export class ReviewsModule {}
